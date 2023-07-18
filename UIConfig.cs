905c83814d892e858dd4d3f6e1a0042b4f6cfaa5
Boojiden <73969201+Boojiden@users.noreply.github.com>
2023-07-18 12:15:41 -0400

    lmao

--- UIConfig.cs
+++ UIConfig.cs
@@ -11,26 +11,16 @@ namespace WingTimeUI
     public class UIConfig : ModConfig
     {
         public override ConfigScope Mode => ConfigScope.ClientSide;
-<<<<<<< Updated upstream
-        
-=======
 
->>>>>>> Stashed changes
         [Range(0f, 1f)]
         [Increment(0.01f)]
         [DefaultValue(0.5f)]
         public float horizontalPercentage;
 
-<<<<<<< Updated upstream
-        
-=======
->>>>>>> Stashed changes
         [Range(0f, 1f)]
         [Increment(0.01f)]
         [DefaultValue(0.7f)]
         public float verticalPercentage;
-<<<<<<< Updated upstream
-=======
 
         [Range(-10000, 10000)]
         [Increment(1)]
@@ -45,7 +35,6 @@ namespace WingTimeUI
         [DefaultValue(true)]
         public bool visible;
 
->>>>>>> Stashed changes
         public override void OnChanged()
         {
             MainUI.ApplyConfigChanges(horizontalPercentage, verticalPercentage);
